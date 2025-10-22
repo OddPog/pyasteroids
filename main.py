2ff0ad55e1fb0dd1ac2c36ff313159611aa3b722
oddpog <48024997+OddPog@users.noreply.github.com>
2025-10-22 12:08:55 -0600

    Attempting to add asteroid field - 1 postional argument error

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_clock = pygame.time.Clock()
+    fps_clock = pygame.time.Clock()
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = updateable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updateable, drawable)
+
     player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player1.update(dt)
+        updateable.update(dt)
         screen.fill("Black")
-        player1.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
-        dt = game_clock.tick(60)/1000
+        dt = fps_clock.tick(60)/1000
 
 
 if __name__ == "__main__":
